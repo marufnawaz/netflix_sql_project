061a3997860a74f67aecc5a1dec00f2c78cd2023
marufnawaz <168166828+marufnawaz@users.noreply.github.com>
2024-10-13 13:08:26 +0530

    Add files via upload

---
+++ netflix_schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE netflix (
+show_id	VARCHAR(6),
+type VARCHAR(10),
+title VARCHAR(160),
+director VARCHAR(210),
+cast VARCHAR(1000),	
+country	VARCHAR(150),
+date_added	VARCHAR(50),
+release_year INT,
+rating VARCHAR(10),	
+duration VARCHAR(15),
+listed_in	VARCHAR(250),
+description VARCHAR(250)
+)
+
